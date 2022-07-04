c7e4044d192e7b3a917797bf35ca7132a1ffe046
cibs84 <d.tilesi@gmail.com>
2022-07-04 18:01:38 +0200

    10 random email created and pushed into emailList

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-            message: "Hello World!!"
+            emailList: [],
+            emailRandom: "",
+        },
+        mounted() {
+            // Creo 10 email random che vado ad inserire nella lista delle email
+            for (let i = 0; i < 10; i++) {
+                const email = this.emailList[i];
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    this.emailRandom = response.data.response
+                    this.emailList.push(this.emailRandom);
+                });
+            }
         }
     }
 );
