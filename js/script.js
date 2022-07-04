0a3698eabd28215e8e7f68524208d57bda015ac8
cibs84 <d.tilesi@gmail.com>
2022-07-04 16:24:10 +0200

    Structures Html and Js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: "Hello World!!"
+        }
+    }
+);
